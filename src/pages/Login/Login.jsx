b921361a4cd5f4fb5210aeb4593e275a743602f3
Marie Otaki <otakimarie@gmail.com>
2021-01-24 11:34:45 -0800

    add fontawesome and adjust the styling

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 import Style from './Login.module.scss';
 import menu from '../../images/menu.svg';
 import profile from '../../images/profile.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
   return (
@@ -28,6 +32,7 @@ const Login = () => {
           <p className={Style.name}>Teacher Name</p>
           <div className={Style.updateButtonContainer}>
             <Link to="/login" className={Style.updateButton}>
+              <FontAwesomeIcon icon={faPen} className={Style.pen} />
               Update Profile Photo
             </Link>
           </div>
@@ -36,8 +41,17 @@ const Login = () => {
       <div className={Style.cardListContainer}>
         <div className={Style.card}>
           <div className={Style.contactInfo}>Contact Information</div>
-          <div className={Style.contactItem}>Phone Number</div>
-          <div className={Style.contactItem}>Email</div>
+          <div className={Style.contactItem}>
+            <FontAwesomeIcon icon={faPhoneAlt} className={Style.phone} />
+            Phone Number
+          </div>
+          <div className={Style.contactItem}>
+            <FontAwesomeIcon
+              icon={faEnvelopeSquare}
+              className={Style.envelope}
+            />
+            Email
+          </div>
         </div>
       </div>
     </div>
