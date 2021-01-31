f47fd1ef83efdc032df001a1d2a593c54a3b04e3
Marie Otaki <otakimarie@gmail.com>
2021-01-30 16:48:06 -0800

    add UserCardContainer component

---
+++ src/components/UserCardContainer/UserCardContainer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button from '../Button/Button';
+import Style from './UserCardContainer.module.scss';
+import userAddressCard from '../../images/userAddressCard.svg';
+
+const UserCardContainer = ({ text }) => {
+  return (
+    <div className={Style.userContainer}>
+      <div className={Style.userContainerLeft}>
+        <div className={Style.userPhoto}></div>
+        <div className={Style.userName}>Name</div>
+      </div>
+      <div className={Style.userContainerRight}>
+        <Button
+          borderColor="#6693B5"
+          backgroundColor="#6693B5"
+          color="#fff"
+          text={text}
+          path="/"
+          width="80px"
+        />
+        <div>
+          <img src={userAddressCard} alt="address-card" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCardContainer;
